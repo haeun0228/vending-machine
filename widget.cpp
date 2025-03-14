6031705d3bde8e5a65cc58f68bf279e033c988f2
haeun0228 <108658648+haeun0228@users.noreply.github.com>
2025-03-14 10:50:25 +0900

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -7,10 +7,11 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    ui->pbCoffee->setEnabled(false);
-    ui->pbMilk->setEnabled(false);
-    ui->pbTea->setEnabled(false);
-    ui->pbReset->setEnabled(false);
+    // ui->pbCoffee->setEnabled(false);
+    // ui->pbMilk->setEnabled(false);
+    // ui->pbTea->setEnabled(false);
+    // ui->pbReset->setEnabled(false);
+    incMoney(0); //ui에서 default 작업 가능 (권장 X) 
 }
 
 Widget::~Widget()
@@ -26,7 +27,7 @@ bool Widget::checkChange(int drink){
 }
 
 
-
+// 함수명 changeMoney로 바꾸기 
 void Widget::incMoney(int button){
     money += button;
     ui->lcdNumber->display(money);
@@ -35,6 +36,7 @@ void Widget::incMoney(int button){
     ui->pbMilk->setEnabled(checkChange(150));
     ui->pbTea->setEnabled(checkChange(200));
     ui->pbReset->setEnabled(money);
+    //assert(moeny>=0) // 반드시 true여야 하는 경우에 assert 사용 
 }
 
 void Widget::on_pb10_clicked()
@@ -42,7 +44,6 @@ void Widget::on_pb10_clicked()
     incMoney(10);
 
 }
-
 
 void Widget::on_pb50_clicked()
 {
@@ -84,6 +85,7 @@ void Widget::on_pbReset_clicked()
 {
     QMessageBox mb;
 
+    //배열과 for문 사용 
     int money500 = money / 500;
     int money100 = (money%500) / 100;
     int money50 = (money%500)%100 / 50;
